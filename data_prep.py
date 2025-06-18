85b64f5f583020a9462c7834b8e29584ddc89475
Tiernan Phillips <139313158+tphi219@users.noreply.github.com>
2025-06-18 23:58:12 +1200

    New data formatting for easier use based on other filtering but puts into new format with rates

--- data_prep.py
+++ data_prep.py
@@ -1,27 +1,77 @@
 import json
+from collections import defaultdict
 
-# Load full JSON
-with open("data1.0.json", encoding="utf-8") as f:
-    full_data = json.load(f)
+def clean_id(item_id):
+    if item_id.startswith("Desc_") and item_id.endswith("_C"):
+        return item_id[5:-2]
+    return item_id
 
-# Extract all recipes
-all_recipes = full_data.get("recipes", {})
+excluded_ingredients = {
+    "Desc_Leaves_C", "Desc_Wood_C", "Desc_HogParts_C", "Desc_StingerParts_C",
+    "Desc_HatcherParts_C", "Desc_SpitterParts_C", "Desc_AlienProtein_C",
+    "Desc_Mycelia_C", "Desc_Biofuel_C", "Desc_LiquidBiofuel_C",
+    "Desc_PackagedBiofuel_C", "Desc_CartridgeStandard_C", "Desc_GenericBiomass_C",
+    "Desc_Crystal_mk2_C", "Desc_Crystal_C", "Desc_Crystal_mk3_C", "Desc_Gunpowder_C",
+}
+excluded_final_products = {
+    "Desc_CompactedCoal_C", "Desc_FluidCanister_C", "Desc_GasTank_C",
+    "Desc_PetroleumCoke_C", "Desc_LiquidFuel_C", "Desc_HeavyOilResidue_C",
+    "Desc_IronScrew_C",
+}
+converter_exception = "Desc_Diamond_C"
 
-# Separate based on the 'alternate' attribute
-alternate_recipes = {}
-normal_recipes = {}
+with open("data1.0_no_screw.json", encoding="utf-8") as f:
+    data = json.load(f)
 
-for key, recipe in all_recipes.items():
-    if recipe.get("inMachine") and not recipe.get("inWorkshop"):
-        if recipe.get("alternate", False):
-            alternate_recipes[key] = recipe
-        else:
-            normal_recipes[key] = recipe
+item_graph = {}
 
-# Save alternate recipes
-with open("alternate_recipes.json", "w", encoding="utf-8") as f:
-    json.dump(alternate_recipes, f, indent=2)
+for recipe in data["recipes"].values():
+    time = recipe.get("time", 1)
+    produced_in = recipe.get("producedIn", [])
+    ingredients = recipe.get("ingredients", [])
+    products = recipe.get("products", [])
 
-# Save normal recipes
-with open("normal_recipes.json", "w", encoding="utf-8") as f:
-    json.dump(normal_recipes, f, indent=2)
+    if not (recipe.get("inMachine") and not recipe.get("inWorkshop") and not recipe.get("alternate")):
+        continue
+    if any(ing["item"] in excluded_ingredients for ing in ingredients):
+        continue
+    if any(prod["item"] in excluded_final_products for prod in products):
+        continue
+    if ("Desc_Packager_C" in produced_in or "Desc_Converter_C" in produced_in) and not any(
+        ing["item"] == converter_exception for ing in ingredients):
+        continue
+
+    for product in products:
+        raw_product_id = product["item"]
+        product_id = clean_id(raw_product_id)
+        product_amt = product["amount"]
+
+        if product_id not in item_graph:
+            item_graph[product_id] = {
+                "name": product_id,
+                "producedFrom": {},
+                "usedIn": {}
+            }
+
+        for ing in ingredients:
+            raw_ing_id = ing["item"]
+            ing_id = clean_id(raw_ing_id)
+            ing_amt = ing["amount"]
+            ing_rate = (ing_amt / time) * 60
+
+            item_graph[product_id]["producedFrom"][ing_id] = ing_rate
+
+            if ing_id not in item_graph:
+                item_graph[ing_id] = {
+                    "name": ing_id,
+                    "producedFrom": {},
+                    "usedIn": {}
+                }
+            item_graph[ing_id]["usedIn"][product_id] = ing_rate
+
+with open("item_dependency_graph.json", "w") as f:
+    json.dump(item_graph, f, indent=2)
+
+# Preview
+print(json.dumps(item_graph["Wire"], indent=2))
+print(json.dumps(item_graph["IronPlateReinforced"], indent=2))
